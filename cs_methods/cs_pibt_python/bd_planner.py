5e1bddf58ce89f03f970e0a6ad9c0cafe5617f94
Arthur Jakobsson <ajakobss@andrew.cmu.edu>
2024-04-03 11:48:14 -0400

    got bds working

---
+++ cs_methods/cs_pibt_python/bd_planner.py
@@ -0,0 +1,132 @@
+from queue import PriorityQueue
+import pdb
+from abc import ABC, abstractmethod
+from collections.abc import Callable
+import numpy as np
+
+class EnvironmentWrapper(ABC):
+    def __init__(self, grid_occupancy) -> None:
+        super().__init__()
+        self.grid_occupancy = grid_occupancy
+
+    def getValid(self, row, col):
+        g_row, g_col = self.grid_occupancy.shape
+        if row>=g_row or col>=g_col or row<0 or col<0:
+            return False
+        return self.grid_occupancy[row, col] == 0
+
+
+class AbstractNode(ABC):
+    def __init__(self, state, parent) -> None:
+        super().__init__()
+        self.state = state
+        self.parent = parent
+
+    @abstractmethod
+    def getSuccessors(self) -> list["AbstractNode"]:
+        # Need "AbstractNode" via https://github.com/microsoft/pylance-release/issues/898
+        pass
+
+    @abstractmethod
+    def getF(self) -> float:
+        pass
+
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+
+    def getState(self):
+        return self.state
+
+    def getParent(self) -> "AbstractNode":
+        return self.parent
+
+    def __lt__(self, other) -> bool:
+        return self.getF() < other.getF()
+
+class GenericPlanner:
+    def __init__(self, environment: EnvironmentWrapper):
+        self.environment = environment
+
+    def findPlan(self, startNode: AbstractNode, goalFunc: Callable[[AbstractNode], bool]):
+                    # getSuccessors: Callable[[AbstractNode], list[AbstractNode]]):
+        open_pq = PriorityQueue()
+        open_pq.put(startNode)
+        closed_set = set()
+
+        num_expansions = 0
+        while not open_pq.empty():
+            current_node : AbstractNode = open_pq.get() # Note get pops the element as well
+            if num_expansions > 5000000:
+                print("Exceeded Expansion Limit")
+                break
+            # print(f"Size: {open_pq.qsize()}")
+
+            if current_node.getState() in closed_set:
+                continue
+            closed_set.add(current_node.getState())
+            if num_expansions % 10000 == 0:
+                print(num_expansions, current_node)
+
+            if goalFunc(current_node):
+                print(f"Found path after {num_expansions} expansions")
+                return self.reconstructPath(current_node)
+
+            num_expansions += 1
+            # pdb.set_trace()
+            # for next_node in getSuccessors(current_node):
+            for next_node in current_node.getSuccessors():
+                open_pq.put(next_node)
+        if open_pq.empty():
+            print(f"Emptied the queue after {num_expansions} expansions")
+        return None
+
+    def reconstructPath(self, node: AbstractNode):
+        """Backtracks to reconstruct the path from the start node to the current node"""
+        path = []
+        while node is not None:
+            path.append(node.getState())
+            node = node.getParent()
+        path.reverse()
+        return path
+
+
+
+def computeHeuristicMap(env: EnvironmentWrapper, goal_xy):
+    """Precompute the heuristic for all states"""
+
+    assert(len(goal_xy) == 2)
+    heuristic_map = np.zeros(env.grid_occupancy.shape)
+
+    class XYNode(AbstractNode):
+        def __init__(self, state, parent, g_val) -> None:
+            super().__init__(state=state, parent=parent)
+            self.g_val = g_val
+
+        # Override
+        def getF(self) -> float:
+            return self.g_val
+
+        def __str__(self) -> str:
+            return f"State: {self.state}, g: {self.g_val}"
+
+        # Override
+        def getSuccessors(self) -> list[AbstractNode]:
+            state = self.getState()
+            assert(len(state) == 2)
+            heuristic_map[state[0], state[1]] = self.g_val  # Set the heuristic value
+            nextNodes = []
+            FOUR_CONNECTED = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+            for dState in FOUR_CONNECTED:
+                new_state = (state[0] + dState[0], state[1] + dState[1])
+                if env.getValid(new_state[0], new_state[1]):
+                    next_node = XYNode(new_state, self, self.g_val+1)
+                    nextNodes.append(next_node)
+            return nextNodes
+
+    gp = GenericPlanner(environment=env)
+    goalFunc = lambda node: False
+    print(goal_xy)
+    startNode = XYNode(goal_xy, None, 0)
+    gp.findPlan(startNode, goalFunc)
+    return heuristic_map
--- cs_methods/cs_pibt_python/cs_pibt.py
+++ cs_methods/cs_pibt_python/cs_pibt.py
@@ -4,6 +4,8 @@ import torch.nn.functional as F
 
 import torch_geometric.nn as pyg_nn
 import torch_geometric.utils as pyg_utils
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import dijkstra
 
 import pdb
 from tqdm import tqdm
@@ -13,6 +15,8 @@ import collections
 import numpy as np
 import torch.optim as optim
 import sys
+
+from bd_planner import *
 
 sys.path.insert(0, '/home/arthur/snap/snapd-desktop-integration/current/Documents/gnn-mapf/gnn/')
 from dataloader import *
@@ -29,10 +33,10 @@ def parse_map(path, mapfile):
     with open(path+mapfile) as f:
         line = f.readline()  # "type octile"
 
-        line = f.readline()  # "height 32"
+        line = f.readline()
         height = int(line.split(' ')[1])
 
-        line = f.readline()  # width 32
+        line = f.readline()
         width = int(line.split(' ')[1])
 
         line = f.readline()  # "map\n"
@@ -40,7 +44,6 @@ def parse_map(path, mapfile):
 
         mapdata = np.array([list(line.rstrip()) for line in f])
 
-    mapdata.reshape((width,height))
     mapdata[mapdata == '.'] = 0
     mapdata[mapdata == '@'] = 1
     mapdata[mapdata == 'T'] = 1
@@ -50,15 +53,15 @@ def parse_map(path, mapfile):
 def linearize_coordinate(row, col, num_of_cols):
     return num_of_cols * row + col
 
-def parse_scene(scen_path, scene_name):
+def parse_scene(scen_path, scene_f):
     start_locations = []
     goal_locations = []
 
-    with open(scen_path+scene_name) as f:
+    with open(scen_path+scene_f) as f:
         line = f.readline().strip()
         # if line[0] == 'v':  # Nathan's benchmark
-        start_locations = np.array([])
-        goal_locations = np.array([])
+        start_locations = list()
+        goal_locations = list()
         sep = "\t"
         for line in f:
             line = line.rstrip() #remove the new line
@@ -68,39 +71,58 @@ def parse_scene(scen_path, scene_name):
             tokens = tokens[4:]  # Skip the first four elements
             col = int(tokens[0])
             row = int(tokens[1])
-            start_locations = np.concatenate((start_locations,np.array([col,row])))
+            start_locations.append(np.array([row,col]))
             col = int(tokens[2])
             row = int(tokens[3])
-            goal_locations = np.concatenate((goal_locations,np.array([col,row])))
-    return (start_locations, goal_locations)
+            goal_locations.append(np.array([row,col]))
+    return (np.array(start_locations), np.array(goal_locations))
 
 
 def parse_scen_name(scen_name):
     index = scen_name.rindex('random')
     return scen_name[0:index-1]
 
+# def get_map_weights(map_arr):
+#     map_arr = map_arr.astype(float)
+#     map_arr[np.where(1==map_arr)]=float('inf')
+#     map_arr+=1
+#     return map_arr
+
+def calculate_bds(goals, map_arr):
+    bds = list()
+    env = EnvironmentWrapper(map_arr)
+    for goal in goals:
+        heuristic = computeHeuristicMap(env, tuple(goal))
+        bds.append(heuristic)
+    return np.array(bds)
+
 class Preprocess():
     def __init__(self, map_files, scen_files, map_path='../map_files/', scen_path = '../scen_files/'):
         self.map_dict = dict()
-        self.map_dict['map'] = collections.defaultdict(list)
-        self.map_dict['scen'] = collections.defaultdict(list)
+        self.map_dict['map'] = dict()
+        self.map_dict['scen'] = dict()
         for map_name in map_files:
            just_map_name = map_name[0:-4]
            print(just_map_name)
            self.map_dict['map'][just_map_name] = parse_map(map_path, map_name)
-        for scen_name in scen_files:
-            just_scen_name = parse_scen_name(scen_name)
-            self.map_dict['scen'][just_scen_name].append(parse_scene(scen_path, scen_name))
+        for scen_f in scen_files:
+            scen_name = parse_scen_name(scen_f)
+            if not scen_name in self.map_dict['scen']:
+                self.map_dict['scen'][scen_name]=collections.defaultdict(list)
+            self.map_dict['scen'][scen_name]['agent_info'].append(parse_scene(scen_path, scen_f))
+
+            #TODO put append back because it should be for the same map that it's a list of scenes
 
     def get_map_dict(self):
         return self.map_dict
 
     def get_start_graph(self):
-
-
-
-
-
+        #get bds using https://judecapachietti.medium.com/dijkstras-algorithm-for-adjacency-matrix-in-python-a0966d7093e8
+        #for all agents in scene get their bd and then use dataloader functions.
+        pdb.set_trace()
+        for scen_name in self.map_dict['scen'].keys():
+            for start_loc, goal_loc in self.map_dict['scen'][scen_name]['agent_info']:
+                self.map_dict['scen'][scen_name]['bd'] = calculate_bds(goal_loc, self.map_dict['map'][scen_name])
 
 if __name__ == "__main__":
     run_name = 'full_sum_aggr'
@@ -111,7 +133,8 @@ if __name__ == "__main__":
     scen_files = ['warehouse-10-20-10-2-2-random-1.scen','warehouse-10-20-10-2-2-random-2.scen']
 
     startup = Preprocess(map_files, scen_files)
-    print(startup.get_map_dict())
+    # print(startup.get_map_dict())
+    startup.get_start_graph()
 
 
 
