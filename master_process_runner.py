63b8dba4842538e0ccf5f78cc37b9eed65a69c5a
kren333 <90985278+kren333@users.noreply.github.com>
2024-06-16 11:17:56 -0700

    initializing files to insert eecbs rerunning

---
+++ master_process_runner.py
@@ -0,0 +1,13 @@
+import os
+
+if __name__ == "__main__":
+    pass
+
+    while True:
+        pass
+
+        # train the naive model
+
+        # run on new maps
+
+        # feed failures into eecbs
