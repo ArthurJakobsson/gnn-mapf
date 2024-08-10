58381624dd7533b118a67d34676027b842158059
Arthur Jakobsson <ajakobss@andrew.cmu.edu>
2024-08-10 17:24:37 -0400

    export intel thing

--- start_sbatch.sh
+++ start_sbatch.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 module load anaconda3
 conda activate arthur_env
+export MKL_SERVICE_FORCE_INTEL=1
 python sbatch_master_process_runner.py --expnum=0 --mini_test=t --expName=EXP_test_sbatch  --data_folder=den312_benchmark --num_parallel=50 --k=4 --m=7 --lr=0.001 --relu_type=leaky_relu --numAgents=100,200,300,400 --which_setting=Arthur --extra_layers=agent_locations --bd_pred=t --which_section=begin --iternum=0
