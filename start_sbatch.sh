9f68e8812e79350d1731ce3df968b60c7b0a6ce2
Arthur Jakobsson <ajakobss@andrew.cmu.edu>
2024-08-18 22:15:42 -0400

    new start sbatch script

--- start_sbatch.sh
+++ start_sbatch.sh
@@ -2,4 +2,4 @@
 module load anaconda3
 conda activate arthur_env
 export MKL_SERVICE_FORCE_INTEL=1
-python sbatch_master_process_runner.py --expnum=0 --mini_test=t --expName=EXP_test_sbatch  --data_folder=den312_benchmark --num_parallel=50 --k=4 --m=7 --lr=0.001 --relu_type=leaky_relu --numAgents=100,200,300,400 --which_setting=Arthur --extra_layers=agent_locations --bd_pred=t --which_section=begin --iternum=0
+python sbatch_master_process_runner.py --expnum=0 --mini_test=t --expName=EXP_large_new_maps  --data_folder=benchmark_large --num_parallel=64 --k=4 --m=5 --lr=0.001 --relu_type=leaky_relu --numAgents=config_example.json --which_setting=Arthur --extra_layers=agent_locations --bd_pred=t --which_section=begin --iternum=0 --percent_for_succ=0.4
