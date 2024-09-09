f43ef79c3a8e34c058aa1753dcb12e887dccf9ce
Arthur Jakobsson <ajakobss@andrew.cmu.edu>
2024-09-08 23:47:52 -0400

    sh changes

--- start_data_sbatch.sh
+++ start_data_sbatch.sh
@@ -2,4 +2,4 @@
 module load anaconda3
 conda activate arthur_env
 export MKL_SERVICE_FORCE_INTEL=1
-python sbatch_data_main_exp.py --expnum=0 --mini_test=t --expName=EXP_large_faster  --data_folder=benchmark_large --num_parallel=64 --k=4 --m=5 --lr=0.001 --relu_type=leaky_relu --numAgents=increment --which_setting=Arthur --extra_layers=agent_locations --bd_pred=t --which_section=begin --iternum=0 --percent_for_succ=0.4 --timeLimit=120 --num_agents_list=[1,2,4,8,16,32,64,128]
+python sbatch_data_main_exp.py --expnum=0 --mini_test=t --expName=EXP_153_scens --data_folder=benchmark_original --num_parallel=64 --k=4 --m=5 --lr=0.001 --relu_type=leaky_relu --numAgents=increment --which_setting=Arthur --extra_layers=agent_locations --bd_pred=t --which_section=begin --iternum=0 --percent_for_succ=0.4 --timeLimit=120 --num_agents_list=1,2,4
