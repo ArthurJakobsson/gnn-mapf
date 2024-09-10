1ffd23370553a36702230f8a66d9be9c49507223
Arthur Jakobsson <ajakobss@andrew.cmu.edu>
2024-09-10 15:57:49 -0400

    cs freeze runner changes

--- sbatch_simulator.py
+++ sbatch_simulator.py
@@ -30,7 +30,7 @@ agent_counts = [8,16,32,64,128]
 LE = "benchmarking"
 command_list = []
 for agent_num in agent_counts:
-    args = f" --mapname=all --numAgents=increment --data_folder=data_collection/data/benchmark_original --map_folder=data_collection/data/benchmark_original/maps --scen_folder=data_collection/data/benchmark_original/scens --extra_layers=agent_locations --model_path=data_collection/data/logs/multi_scen_runs/{agent_num}/models/max_test_acc.pt --num_parallel=64 --bd_pred=t --eecbs_cutoff=120 --simulator_cutoff=120 --pymodel_out=benchmarking/{agent_num}_agents_results"
+    args = f" --mapname=all --numAgents=increment --data_folder=data_collection/data/benchmark_original --map_folder=data_collection/data/benchmark_original/maps --scen_folder=data_collection/data/benchmark_original/scens --extra_layers=agent_locations --model_path=data_collection/data/logs/multi_scen_runs/{agent_num}/models/max_test_acc.pt --num_parallel=64 --bd_pred=t --eecbs_cutoff=120 --simulator_cutoff=120 --pymodel_out=benchmarking/{agent_num}_CSFreeze_results --shieldType=CS-Freeze"
     generate_sh_script(LE, f"run_{agent_num}", "benchmarking.benchmark_runner", args)
     command = [
             'sbatch',
